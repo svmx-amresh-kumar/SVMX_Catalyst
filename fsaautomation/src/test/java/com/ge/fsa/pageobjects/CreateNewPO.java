3753d1f9e3d88c5272cf231c18b19a9702087325
meghanarao-servicemax <meghana.rao@servicemax.com>
2018-07-09 11:23:42 +0530

    pushing changes

---
+++ fsaautomation/src/test/java/com/ge/fsa/pageobjects/CreateNewPO.java
@@ -0,0 +1,72 @@
+/*
+ *  @author MeghanaRao
+ */
+package com.ge.fsa.pageobjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+
+import com.kirwa.nxgreport.NXGReports;
+import com.kirwa.nxgreport.logging.LogAs;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
+
+public class CreateNewPO 
+{
+	public CreateNewPO(AppiumDriver driver)
+	{
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+	AppiumDriver driver = null;
+	TouchAction touchAction = null;
+	
+	@FindBy(xpath="//div[@class='svmx-menu-icon-label'][text()='Create New']")
+	private WebElement eleCreateNew;
+	public WebElement getEleCreateNew()
+	{
+		return eleCreateNew;
+	}
+	
+	private WebElement eleProductNameTxt;
+	public WebElement getEleProductNameTxt(String sProductName)
+	{
+		eleProductNameTxt=driver.findElement(By.xpath("//div[@class='x-inner-el'][text()='"+sProductName+"']"));
+		return eleProductNameTxt;
+	}
+	
+	@FindBy(xpath="//*[. = 'Activity Type']//input")
+	private WebElement eleActivityTypeLst;
+	public WebElement getEleActivityTypeLst()
+	{
+		return eleActivityTypeLst;
+	}
+	
+	@FindBy(xpath="//div[text()='Create Work Order']")
+	private WebElement eleCreateNewWorkOrder;
+	public WebElement getEleCreateNewWorkOrder()
+	{
+		return eleCreateNewWorkOrder;
+	}
+	
+	
+	
+	
+	//To add Travel
+		public void addTravel(CommonsPO commonsPo, WorkOrderPO workOrderPo) throws InterruptedException
+		{	//Adding labor parts name
+			commonsPo.tap(workOrderPo.getEleAddTravelLnk());
+		
+			workOrderPo.setTime(commonsPo, workOrderPo.getEleStartDateTimeLst(), 1, "5");  // Sets start date time
+			workOrderPo.setTime(commonsPo, workOrderPo.getEleEndDateTimeLst(), 1, "9");    // Sets end date time
+			
+
+			//Verify to Manage WO lines
+			Assert.assertTrue(workOrderPo.getEleManageWOLinesTxt().isDisplayed(), "Failed to add Labor parts");   
+			NXGReports.addStep("Labor parts are added and saved successfully. ", LogAs.PASSED, null);		
+		}
+}
---
+++ fsaautomation/src/test/java/com/ge/fsa/tests/Scenario1.java
@@ -0,0 +1,70 @@
+/*
+ *  @author MeghanaRao
+ */
+package com.ge.fsa.tests;
+import org.testng.annotations.Test;
+import java.io.IOException;
+import org.testng.annotations.BeforeMethod;
+import com.ge.fsa.lib.RestServices;
+import com.ge.fsa.lib.BaseLib;
+import com.ge.fsa.lib.GenericLib;
+import com.ge.fsa.pageobjects.ExploreSearchPO;
+import com.ge.fsa.pageobjects.LoginHomePO;
+import com.ge.fsa.pageobjects.CommonsPO;
+import com.ge.fsa.pageobjects.CreateNewPO;
+import com.ge.fsa.pageobjects.ToolsPO;
+import com.ge.fsa.pageobjects.WorkOrderPO;
+import com.kirwa.nxgreport.NXGReports;
+import com.kirwa.nxgreport.logging.LogAs;
+import com.kirwa.nxgreport.selenium.reports.CaptureScreen;
+import com.kirwa.nxgreport.selenium.reports.CaptureScreen.ScreenshotOf;
+
+
+public class Scenario1 extends BaseLib
+{
+	GenericLib genericLib = null;
+	RestServices restServices = null;
+	LoginHomePO loginHomePo = null;
+	ExploreSearchPO exploreSearchPo = null;	
+	WorkOrderPO workOrderPo = null;
+	CommonsPO commonsPo = null;
+	CreateNewPO createNewPO = null;
+	ToolsPO toolsPo=null;
+	int iWhileCnt =0;
+	String sTestCaseID=null; String sCaseWOID=null; String sCaseSahiFile=null;
+	String sExploreSearch=null;String sWorkOrderID=null; String sWOJsonData=null;String sWOName=null; String sFieldServiceName=null; String sProductName1=null;String sProductName2=null; 
+	String sActivityType=null;String sPrintReportSearch=null;
+	@BeforeMethod
+	public void initializeObject() throws IOException
+	{	// Initialization of objects
+		genericLib=new GenericLib();
+		restServices = new RestServices();
+		
+		loginHomePo = new LoginHomePO(driver);
+		exploreSearchPo = new ExploreSearchPO(driver);	
+		workOrderPo = new WorkOrderPO(driver);
+		toolsPo = new ToolsPO(driver);	
+		commonsPo = new CommonsPO(driver);
+		createNewPO = new CreateNewPO(driver);
+		restServices.getAccessToken();
+		//Creation of dynamic Work Order
+		sWOJsonData="{\"SVMXC__City__c\":\"Delhi\",\"SVMXC__Zip__c\":\"110003\",\"SVMXC__Country__c\":\"India\",\"SVMXC__State__c\":\"Haryana\"}";	
+		
+		sWorkOrderID=restServices.getWOORecordID(sWOJsonData);
+		sWOName =restServices.getWOName(sWorkOrderID);
+				
+		//Pre Login to app
+	    loginHomePo.login(commonsPo,exploreSearchPo);
+	}
+	
+	@Test
+	public void scenario1() throws Exception
+	{
+		
+		System.out.println("Automation : "+createNewPO.getEleCreateNew().getLocation());
+		commonsPo.tap(createNewPO.getEleCreateNew());
+		commonsPo.tap(createNewPO.getEleCreateNewWorkOrder());
+		
+	}
+	
+}
