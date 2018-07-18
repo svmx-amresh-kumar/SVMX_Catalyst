1c47d37fc5e7fde568e4c3a97389f070e1583c50
meghanarao-servicemax <meghana.rao@servicemax.com>
2018-07-18 14:56:49 +0530

    removing executable folder as it is created on the fly

--- Executable/sahiExecutable.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash 
-cd /auto/sahi_pro/userdata/bin 
-./testrunner.sh /auto/sahi_pro/userdata/scripts/Sahi_Project_Lightning/svmx/test_lab/test_cases/backOffice/appium_verifyWorkDetails.sah https://test.salesforce.com chrome
