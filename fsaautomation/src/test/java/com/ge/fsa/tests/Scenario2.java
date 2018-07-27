84249bee2d0a697d176c1dbc6e605a4dd1508d40
vinod <vinod.tharavath@ge.com>
2018-07-27 15:28:37 +0530

    updating config,and scenario2

--- fsaautomation/src/test/java/com/ge/fsa/tests/Scenario2.java
+++ fsaautomation/src/test/java/com/ge/fsa/tests/Scenario2.java
@@ -2,6 +2,9 @@ package com.ge.fsa.tests;
 
 import java.io.IOException;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -20,10 +23,10 @@ import com.kirwa.nxgreport.logging.LogAs;
 import com.kirwa.nxgreport.selenium.reports.CaptureScreen;
 import com.kirwa.nxgreport.selenium.reports.CaptureScreen.ScreenshotOf;
 
-public class Scenario2 extends BaseLib{
-	
-	
-	
+import io.appium.java_client.TouchAction;
+
+public class Scenario2 extends BaseLib {
+
 	int iWhileCnt = 0;
 	String sTestCaseID = null;
 	String sCaseWOID = null;
@@ -42,8 +45,7 @@ public class Scenario2 extends BaseLib{
 	@BeforeMethod
 	public void initializeObject() throws Exception { // Initialization of objects
 
-
-}
+	}
 
 	@Test(enabled = true)
 	public void scenario2_checklist() throws Exception {
@@ -59,94 +61,115 @@ public class Scenario2 extends BaseLib{
 		sExploreSearch = GenericLib.getExcelData(sTestCaseID, "ExploreSearch");
 		sFieldServiceName = GenericLib.getExcelData(sTestCaseID, "ProcessName");
 		sChecklistName = GenericLib.getExcelData(sTestCaseID, "ChecklistName");
-		
+
 		restServices.getAccessToken();
 		// Creation of dynamic Work Order
 		sWOJsonData = "{\"SVMXC__City__c\":\"Delhi\",\"SVMXC__Zip__c\":\"110003\",\"SVMXC__Country__c\":\"India\",\"SVMXC__State__c\":\"Haryana\"}";
 		sWorkOrderID = restServices.getWOORecordID(sWOJsonData);
-	sWOName = restServices.getWOName(sWorkOrderID);
-		//sWOName="WO-00000166";
-	String ChecklistTextQuestion ="text2372018162553";
-			
+		sWOName = restServices.getWOName(sWorkOrderID);
+		// sWOName="WO-00000269";
+		String ChecklistTextQuestion = "text2372018162553";
 
 		GenericLib.setCongigValue(GenericLib.sDataFile, sCaseWOID, sWOName);
 		try {
-			
 
 			// Pre Login to app
 			loginHomePo.login(commonsPo, exploreSearchPo);
-			toolsPo.configSync(commonsPo);
-			System.out.println("Config Sync completed.....");
-			toolsPo.syncData(commonsPo);
-			
+			//toolsPo.configSync(commonsPo);
+			toolsPo.syncData(commonsPo);	
 			commonsPo.tap(exploreSearchPo.getEleExploreIcn());
 			// Explore and Navigate to the Search Process
 			commonsPo.getSearch(exploreSearchPo.getEleSearchNameTxt(sExploreSearch));
 			exploreSearchPo.getEleSearchNameTxt(sExploreSearch).click();
 			commonsPo.longPress(exploreSearchPo.getEleSearchNameTxt(sExploreSearch));
-			// Select the Work Order
+			// // Select the Work Order
 			exploreSearchPo.selectWorkOrder(commonsPo, sWOName);
-			// Navigate to Field Service
-			workOrderPo.selectAction(commonsPo, sFieldServiceName);	
-			//Navigating to the checklist		
-			Thread.sleep(2000);
+			// // Navigate to Field Service
+			workOrderPo.selectAction(commonsPo, sFieldServiceName);
+			// Navigating to the checklist
+			Thread.sleep(5000);
+		//	System.out.println(checklistPo.geteleStartNew());
+			/*
+			 * if (checklistPo.geteleStartNew() != null) {
+			 * System.out.println("entered loop -now click start new");
+			 * commonsPo.tap(checklistPo.geteleStartNew(),15,18);
+			 * 
+			 * 
+			 * //checklistPo.geteleStartNew().click(); //
+			 * System.out.println(checklistPo.geteleStartNew().getLocation()); // Point p =
+			 * checklistPo.geteleStartNew().getLocation(); // p.x = p.x*2; //
+			 * System.out.println("loc"+p); // p.move(p.x, p.y); // // //
+			 * //commonsPo.longPress(checklistPo.geteleStartNew()); //
+			 * commonsPo.tap(checklistPo.geteleStartNew(),p.x,p.y); } else
+			 */
 			commonsPo.longPress(checklistPo.geteleChecklistName(sChecklistName));
 			Thread.sleep(2000);
 			checklistPo.geteleChecklistAnswerTextArea(ChecklistTextQuestion).sendKeys("Text Question Answered");
-			
-			//tapping the next button in checklist
+
+			// tapping the next button in checklist
 			commonsPo.tap(checklistPo.eleNext());
-			//submitting the checklist
+			// submitting the checklist
 			Thread.sleep(2000);
 			commonsPo.tap(checklistPo.eleChecklistSubmit());
-		
-			//tapping on the validation sucessfull checklist popup		   
-		   
-		   commonsPo.longPress(checklistPo.eleChecklistPopupSubmit());
-		    System.out.println("finished clicking on submit popup.");
-		 
+
+			// tapping on the validation sucessfull checklist popup
+
+			commonsPo.longPress(checklistPo.eleChecklistPopupSubmit());
+			System.out.println("finished clicking on submit popup.");
+
+			// Tapping on Show Completed Checklists
+			System.out.println("going to tap on show completedchecklists");
+			commonsPo.longPress(checklistPo.eleShowCompletedChecklist());
+			System.out.println("tapped on completed checklist");
+			System.out.println("going to tap on the completedchecklist");
+			commonsPo.tap(checklistPo.eleCompletedChecklistName(sChecklistName));
+			//System.out.println(checklistPo.eleCompletedChecklistName(sChecklistName));
+			System.out.println("tapped on completed checklist");
+		//	System.out.println("printing checklisttextquesion" + ChecklistTextQuestion);
+		//	Thread.sleep(5000);
+			System.out.println("=============================================");
+			//checklistPo.geteleChecklistAnswerTextArea(ChecklistTextQuestion)ChecklistTextQuestion.click();
+			String qans = checklistPo.geteleChecklistAnswerTextArea(ChecklistTextQuestion).getText();
+			System.out.println("trying with eleone----.......");
+			
+			//checklistPo.geteleChecklistAnswerTextArea(ChecklistTextQuestion);
+			System.out.println(checklistPo.getText(driver, checklistPo.geteleChecklistAnswerTextArea(ChecklistTextQuestion)));
+			WebElement ws = checklistPo.getelecsstextarea2();
+			System.out.println(checklistPo.getelecsstextarea2().getText());
+			
+		  //  System.out.println("vinaya printing ws"+ws);
+		    checklistPo.expandRootElement(ws);
+		    System.out.println("post expanding root");
+		    System.out.println(ws);
+		    System.err.println("-----------------");
+		    System.out.println(ws.getText());
+		    System.out.println("innerhtml------");
+		    System.out.println(ws.getAttribute("innerHTML"));
+		    System.out.println("----innertext-----");		
+		    System.out.println(ws.getAttribute("innerText"));
 		    
-		    //Tapping on Show Completed Checklists
-		    System.out.println("going to tap on show completedchecklists");
-		    commonsPo.longPress(checklistPo.eleShowCompletedChecklist());
-		    System.out.println("tapped on completed checklist");
-	  
 		    
-		   System.out.println("going to tap on the completedchecklist");
-		   System.out.println(driver.getPageSource());
-		    commonsPo.tap(checklistPo.eleCompletedChecklistName(sChecklistName));
-		    System.out.println(checklistPo.eleCompletedChecklistName(sChecklistName));
-		    System.out.println("tapped on completed checklist");
-		    String qans = checklistPo.geteleChecklistAnswerTextArea(ChecklistTextQuestion).getText();
-		    System.out.println("tesx====="+qans);
-		    if (qans == "Text Question Answered") {
-				System.out.println("Answered value sucessfull!");
-			}
+			//System.out.println(checklistPo.getshadowroot(ChecklistTextQuestion).getText());;
+			//WebElement shadowroot = (WebElement) ((JavascriptExecutor)driver) .executeScript("return arguments[0].shadowRoot",checklistPo.geteleChecklistAnswerTextArea(ChecklistTextQuestion));
+			//System.out.println("shadowroot.text"+shadowroot.getText());
+			//*[@id="ext-element-1731"]//div
 			// Sync the Data
-			//	toolsPo.syncData(commonsPo);
+			// toolsPo.syncData(commonsPo);
 			// Execute Sahi for server side validation
 			genericLib.executeSahiScript(GenericLib.getCongigValue(GenericLib.sDataFile, "RS_2389_SAHISCRIPT"),
 					sTestCaseID);
 			NXGReports.addStep("Testcase " + sTestCaseID + " PASSED", LogAs.PASSED, null);
-		} 
-		catch (Exception e) {
-			//NXGReports.addStep("Testcase " + sTestCaseID + " FAILED", LogAs.FAILED,new CaptureScreen(ScreenshotOf.BROWSER_PAGE));
+		} catch (Exception e) {
+			// NXGReports.addStep("Testcase " + sTestCaseID + " FAILED", LogAs.FAILED,new
+			// CaptureScreen(ScreenshotOf.BROWSER_PAGE));
 			throw e;
 		}
 
 	}
 
-	
 	@AfterMethod
-		public void tearDownDriver()
-		{
-			driver.quit();
-		}
-		
-		
+	public void tearDownDriver() {
+		driver.quit();
+	}
+
 }
-	
-	
-	
-
-
---
+++ fsaautomation/src/test/java/com/ge/fsa/tests/Scenario7.java
@@ -0,0 +1,128 @@
+package com.ge.fsa.tests;
+
+import org.json.JSONArray;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.ge.fsa.lib.BaseLib;
+import com.ge.fsa.lib.GenericLib;
+import com.kirwa.nxgreport.NXGReports;
+import com.kirwa.nxgreport.logging.LogAs;
+
+public class Scenario7 extends BaseLib{
+	String sTestCaseID= null;
+	String sCaseWOID = null;
+	String sExploreSearch = null;
+	String sChecklistName = null;
+	String sFieldServiceName = null;
+	String sWorkOrderID = null;
+	String sWOJsonData = null;
+	String sWOName = null;
+	String sChecklistOpDocName = null;
+	
+	
+	@Test(enabled = true)
+	public void scenario7() throws Exception {
+		sTestCaseID = "Scenario7_Checklist";
+		sCaseWOID = "Data_Scenario7_Checklist";
+		
+		
+		
+		
+		sExploreSearch = GenericLib.getExcelData(sTestCaseID, "ExploreSearch");
+		sFieldServiceName = GenericLib.getExcelData(sTestCaseID, "ProcessName");
+		sChecklistName = GenericLib.getExcelData(sTestCaseID, "ChecklistName");
+		sChecklistOpDocName =GenericLib.getExcelData(sTestCaseID, "ChecklistOpDocName");
+		
+		
+		//NEW REST 
+		/*JSONArray sJsonArrayWo = restServices.restCreate("SVMXC__Service_Order__c?","{\"SVMXC__City__c\":\"Delhi\",\"SVMXC__Zip__c\":\"110003\",\"SVMXC__Country__c\":\"India\",\"SVMXC__State__c\":\"Haryana\"}");
+		String sWORecordID = restServices.getJsonValue(sJsonArrayWo, "id");
+		
+		System.out.println(sWORecordID);
+		String sWOName = restServices.restGetSoqlValue("SELECT+name+from+SVMXC__Service_Order__c+Where+id+=\'"+sWORecordID+"\'", "name");
+
+		System.out.println("WO no ="+sWOName);*/
+
+		String ChecklistTextQuestion = "What is the Work Order Number?";
+		String ChecklistrequestionQ = "This is a required question";
+		
+		
+	restServices.getAccessToken();
+		sWOJsonData = "{\"SVMXC__City__c\":\"Delhi\",\"SVMXC__Zip__c\":\"110003\",\"SVMXC__Country__c\":\"India\",\"SVMXC__State__c\":\"Haryana\"}";
+		sWorkOrderID = restServices.getWOORecordID(sWOJsonData);
+		sWOName = restServices.getWOName(sWorkOrderID);
+		//sWOName= "WO-00000366";
+		
+	//vt to learn!---GenericLib.setCongigValue(GenericLib.sDataFile, sCaseWOID, sWOName);
+		
+		
+		loginHomePo.login(commonsPo, exploreSearchPo);
+		//toolsPo.configSync(commonsPo);
+		toolsPo.syncData(commonsPo);	
+		commonsPo.tap(exploreSearchPo.getEleExploreIcn());
+		// Explore and Navigate to the Search Process
+		commonsPo.getSearch(exploreSearchPo.getEleSearchNameTxt(sExploreSearch));
+		exploreSearchPo.getEleSearchNameTxt(sExploreSearch).click();
+		commonsPo.longPress(exploreSearchPo.getEleSearchNameTxt(sExploreSearch));
+		// // Select the Work Order
+		exploreSearchPo.selectWorkOrder(commonsPo, sWOName);
+		// // Navigate to Field Service
+		workOrderPo.selectAction(commonsPo, sFieldServiceName);
+		// Navigating to the checklist
+		commonsPo.longPress(checklistPo.geteleChecklistName(sChecklistName));
+		Thread.sleep(2000);
+		
+		//TRIES FOR SHADOW
+		
+		
+		System.out.println(checklistPo.geteleChecklistAnswerTextArea(ChecklistTextQuestion).getText());
+		checklistPo.geteleChecklistAnswerTextArea(ChecklistTextQuestion).click();
+		System.out.println(checklistPo.geteleChecklistAnswerTextArea(ChecklistTextQuestion).getText());
+		System.out.println(checklistPo.geteleChecklistAnswerTextArea(ChecklistTextQuestion).getTagName());
+		commonsPo.tap(checklistPo.geteleChecklistAnswerTextArea(ChecklistTextQuestion));
+		System.out.println("tapped");
+		commonsPo.longPress(checklistPo.geteleChecklistAnswerTextArea(ChecklistTextQuestion));
+		System.out.println(checklistPo.geteleChecklistAnswerTextArea(ChecklistTextQuestion).getText());
+		System.out.println("longpressed");
+		System.out.println(checklistPo.geteleChecklistAnswerTextArea(ChecklistTextQuestion).getText());
+		//checklistPo.geteleChecklistAnswerTextArea(ChecklistTextQuestion).sendKeys("hello");
+		System.out.println(checklistPo.geteleChecklistAnswerTextArea(ChecklistTextQuestion).isEnabled());
+		System.out.println(checklistPo.geteleChecklistAnswerTextArea(ChecklistTextQuestion).getCssValue("contenteditbale"));		
+		
+		
+		
+		//tapping next button
+		commonsPo.tap(checklistPo.eleNext());
+		// submitting the checklist
+		Thread.sleep(2000);
+		commonsPo.tap(checklistPo.eleChecklistSubmit());
+		
+		//Validation of required question lbl and issue found txt.
+		Thread.sleep(2000);
+		Assert.assertTrue(checklistPo.getelefillrequiredfieldlbl().isDisplayed(),"Failed to provide:Please fill this required field and submit again-checklist");
+		NXGReports.addStep("checklistrequiredquestion passed", LogAs.PASSED, null);
+		commonsPo.waitforElement(checklistPo.geteleissuefoundlbl(),1000);
+		Assert.assertTrue(checklistPo.geteleissuefoundlbl().isDisplayed(),"Failed to display issue found for required question-checklist");
+		NXGReports.addStep("checklistrequiredquestion passed", LogAs.PASSED, null);
+		checklistPo.geteleChecklistrequiredTxt(ChecklistrequestionQ).sendKeys("required answer");
+		
+		
+		//submitting of checklist
+		
+		commonsPo.tap(checklistPo.eleChecklistSubmit());
+		commonsPo.longPress(checklistPo.eleChecklistPopupSubmit());
+		
+		
+		//Navigating back to work Orders
+		commonsPo.tap(checklistPo.geteleBacktoWorkOrderlnk());		
+			
+		
+		//Navigating to the checklist OP
+		workOrderPo.selectAction(commonsPo, sChecklistOpDocName);
+		checklistPo.validateChecklistServiceReport(commonsPo, workOrderPo, sChecklistOpDocName,sWOName );
+		
+		
+		
+	}
+}
