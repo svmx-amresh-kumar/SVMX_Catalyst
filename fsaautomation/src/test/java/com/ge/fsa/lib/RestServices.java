cadbeebcf5714c37734547003392c452ba490ac5
Yadav Thyagaraj <yadav.thyagaraj@servicemax.com>
2018-08-01 12:16:45 +0530

    new commit updated rest api services

--- fsaautomation/src/test/java/com/ge/fsa/lib/RestServices.java
+++ fsaautomation/src/test/java/com/ge/fsa/lib/RestServices.java
@@ -83,96 +83,7 @@ public class RestServices
 		
 	}
 	
-	//**Commenting out as we can use restCreate() and getJsonValue() in test case directly for any field and not just work order.
 	
-//To Fetch Work Order ID
-public  String getWOORecordID(String sWOJson) throws IOException
-{
-	URL url = new URL(GenericLib.getCongigValue(GenericLib.sConfigFile, "WORECORD_URL")
-			+ "Username="+GenericLib.getCongigValue(GenericLib.sConfigFile, "ADMIN_USN")
-			+ "&Password="+GenericLib.getCongigValue(GenericLib.sConfigFile, "ADMIN_PWD"));
-    HttpsURLConnection httpsUrlCon = (HttpsURLConnection) url.openConnection();
-    httpsUrlCon.setDoOutput(true);
- 	httpsUrlCon.setRequestMethod("POST");
-	httpsUrlCon.setRequestProperty("Content-Type", "application/json");
-	httpsUrlCon.setRequestProperty("Authorization", "OAuth "+sAccessToken);
-	
-	OutputStream os = httpsUrlCon.getOutputStream();
-    os.write(sWOJson.getBytes());
-    os.flush();
-	
-    BufferedReader bufferedReader = null;
-    StringBuilder stringBuilder = new StringBuilder();
-    String line;
-    try {
-           bufferedReader = new BufferedReader(new InputStreamReader(httpsUrlCon.getInputStream()));
-           while ((line =bufferedReader.readLine())!=null){
-                 stringBuilder.append(line);
-           }
-    } catch (IOException e) {
-           e.printStackTrace();
-    } finally {
-           if (bufferedReader != null) {
-                 try {
-                        bufferedReader.close();
-                 } catch (IOException e) {
-                        e.printStackTrace();
-                 }
-           }
-    }
-
-    JSONObject json = new JSONObject(stringBuilder.toString());
-    sWORecordID = (String) json.get("id");
-    System.out.println("WORecordID: "+sWORecordID);
-    return sWORecordID;
-}
-
-	// To fetch WorkOrder Name
-	public String  getWOName(String sWORecordID) throws IOException
-	{
-	String sURL = GenericLib.getCongigValue(GenericLib.sConfigFile, "WONAME_URL")+"SELECT+name+from+SVMXC__Service_Order__c+Where+id+=\'"+sWORecordID+"\'";
-	URL url = new URL(sURL);
-	System.out.println(sURL);
-	HttpsURLConnection httpsUrlCon = (HttpsURLConnection) url.openConnection();
-	httpsUrlCon.setDoOutput(true);
-	httpsUrlCon.setRequestMethod("GET");
-	httpsUrlCon.setRequestProperty("Authorization", "OAuth "+sAccessToken);
-	httpsUrlCon.setRequestProperty("Username",GenericLib.getCongigValue(GenericLib.sConfigFile, "ADMIN_USN") );
-	httpsUrlCon.setRequestProperty("Password", GenericLib.getCongigValue(GenericLib.sConfigFile, "ADMIN_PWD"));
-	
-	BufferedReader bufferedReader = null;
-	StringBuilder stringBuilder = new StringBuilder();
-	String line;
-	try {
-	   bufferedReader = new BufferedReader(new InputStreamReader(httpsUrlCon.getInputStream(),StandardCharsets.UTF_8));
-	   while ((line =bufferedReader.readLine())!=null){
-	         stringBuilder.append(line);
-	   }
-	} catch (IOException e) {
-	   e.printStackTrace();
-	} finally {
-	   if (bufferedReader != null) {
-	         try {
-	                bufferedReader.close();
-	         } catch (IOException e) {
-	                e.printStackTrace();
-	         }
-	   }
-	}
-	
-	JSONObject json = new JSONObject(stringBuilder.toString());
-	
-	JSONArray msg = (JSONArray) json.get("records");
-	Iterator iterator = msg.iterator();
-	while (iterator.hasNext()) {
-         JSONObject value = (JSONObject) iterator.next();
-         System.out.println((String) value.get("Name"));
-         
-         sWorkOrderName=(String) value.get("Name");
-     }
-	
-	return sWorkOrderName;
-	}
 	
 	/**
 	 * Method to return the JSONArray, to be used with getJsonValue() method for multiple value extraction after a single API call
@@ -190,6 +101,7 @@ public  String getWOORecordID(String sWOJson) throws IOException
 	 */
 	public JSONArray restGetSoqlJsonArray(String soqlquery ) throws IOException
 	{
+		soqlquery = parseQuery(soqlquery);
 		String sURL = GenericLib.getCongigValue(GenericLib.sConfigFile, "WONAME_URL")+soqlquery;
 		URL url = new URL(sURL);
 		System.out.println(sURL);
@@ -233,7 +145,6 @@ public  String getWOORecordID(String sWOJson) throws IOException
 
 	
 	/**
-	 * Author : Meghana Rao
 	 * 
 	 * This method will query other objects and will send the values which is requested by us
 	 * 
@@ -245,6 +156,9 @@ public  String getWOORecordID(String sWOJson) throws IOException
 	
 	public String restGetSoqlValue(String soqlquery , String getvalue) throws IOException
 	{
+		
+		soqlquery = parseQuery(soqlquery);
+		
 		String sURL = GenericLib.getCongigValue(GenericLib.sConfigFile, "WONAME_URL")+soqlquery;
 		URL url = new URL(sURL);
 		System.out.println(sURL);
@@ -284,7 +198,7 @@ public  String getWOORecordID(String sWOJson) throws IOException
 		if(getvalue == "totalSize")
 		{
 		String msgString = json.get(getvalue).toString();
-		System.out.println("msgString "+msgString);
+	 	System.out.println("Returned value for "+getvalue+" = "+msgString);
 		returnvalue= msgString;
 		
 		}
@@ -387,6 +301,17 @@ public  String getWOORecordID(String sWOJson) throws IOException
 
 		return msg;
 	 }
+	 
+	 /**
+	  * Return a new query after adding \"+\" to spaces
+	  * @param query
+	  * @return
+	  */
+	 public String parseQuery(String query) {
+			query = query.replaceAll("\\s","+");
+			System.out.println("New query after adding \"+\" to spaces = "+query);
+			return query;
+		}
 
 //		/**
 //		 * 
