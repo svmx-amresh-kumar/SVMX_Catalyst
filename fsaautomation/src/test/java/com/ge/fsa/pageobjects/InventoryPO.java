ed3bc2483d7c7fa184b9167af5ad1f874c455763
vinod <vinod.tharavath@ge.com>
2018-08-07 11:53:49 +0530

    pushing Scenario9 and FSA app version 3.3.3 build 420

---
+++ fsaautomation/src/test/java/com/ge/fsa/pageobjects/InventoryPO.java
@@ -0,0 +1,41 @@
+package com.ge.fsa.pageobjects;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import io.appium.java_client.AppiumDriver;
+
+
+public class InventoryPO {
+	
+	public InventoryPO(AppiumDriver driver) 
+	{
+	this.driver = driver;
+	PageFactory.initElements(driver, this);
+	}
+	AppiumDriver driver = null;
+	
+	@FindBy(xpath="//div[@class='svmx-menu-icon-label'][text()='Parts']")
+	private WebElement elePartsIcn;
+	public WebElement getelePartsIcn()
+	{
+		return elePartsIcn;
+	}
+	
+	@FindBy(xpath="//*[text() = 'My Stock']")
+	private WebElement eleMyStockTab;
+	public WebElement geteleMyStockTab()
+	{
+		return eleMyStockTab;
+	}
+	
+	@FindBy(xpath="//*[text() = 'Catalog']")
+	private WebElement eleCatalogTab;
+	public WebElement geteleCatalogTab()
+	{
+		return eleCatalogTab;
+	}
+	
+
+}
