68ef4dfe0814ac745319f8c8b44f727bb3de45bb
Yadav Thyagaraj <yadav.thyagaraj@servicemax.com>
2018-09-10 11:35:52 +0530

    renamed

--- fsaautomation/src/test/java/com/ge/fsa/tests/SCN_CreateNewRecord_RS_10570.java
+++ fsaautomation/src/test/java/com/ge/fsa/tests/SCN_CreateNewRecord_RS_10570.java
@@ -4,10 +4,10 @@ import org.testng.annotations.Test;
 
 import com.ge.fsa.lib.BaseLib;
 
-public class Scenario10570Test extends BaseLib {
+public class SCN_CreateNewRecord_RS_10570 extends BaseLib {
 
 	@Test
-	public void scenario10570Test() throws InterruptedException {
+	public void RS_10570Test() throws InterruptedException {
 		loginHomePo.login(commonsPo, exploreSearchPo);
 		toolsPo.syncData(commonsPo);
 		createNewPO.getEleCreateNew().click();
