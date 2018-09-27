3c09d067c2a0491845a9a87ef2ba7de08adfc659
harishsvmx <harish.cs@ge.com>
2018-09-27 15:29:26 +0530

    Pushing changes

---
+++ fsaautomation/src/test/java/com/ge/fsa/tests/SCN_Lookups_4_10530.java
@@ -0,0 +1,20 @@
+package com.ge.fsa.tests;
+
+import org.testng.annotations.Test;
+
+import com.ge.fsa.lib.BaseLib;
+import com.ge.fsa.lib.GenericLib;
+
+public class SCN_Lookups_4_10530 extends BaseLib {
+	
+	@Test
+	public void RS_10530() throws InterruptedException {
+		loginHomePo.login(commonsPo, exploreSearchPo);	
+//		toolsPo.syncData(commonsPo);
+		Thread.sleep(GenericLib.iMedSleep);
+		workOrderPo.navigateToWOSFMWithIcon(commonsPo, exploreSearchPo, "AUTOMATION SEARCH", "Work Orders", "WO-00002005", "10530_lkp_proc1");
+		Thread.sleep(GenericLib.iHighSleep);
+		
+	}
+
+}
