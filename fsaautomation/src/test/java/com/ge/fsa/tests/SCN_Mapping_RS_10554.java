a4453454c0bf14f89f65b60c4c2c4726759a3f48
yadavsvmx <yadav.thyagaraj@ge.com>
2018-10-16 15:40:42 +0530

    new

--- fsaautomation/src/test/java/com/ge/fsa/tests/SCN_Mapping_RS_10554.java
+++ fsaautomation/src/test/java/com/ge/fsa/tests/SCN_Mapping_RS_10554.java
@@ -175,7 +175,7 @@ public class SCN_Mapping_RS_10554 extends BaseLib {
 			commonsPo.tap(workOrderPo.getEleSaveLnk());
 			
 			toolsPo.syncData(commonsPo);
-			Thread.sleep(GenericLib.iMedSleep);
+			//Thread.sleep(GenericLib.iMedSleep);
 			
 			// Collecting the Work Order number from the Server.
 			String sSoqlQuery = "SELECT+name+from+SVMXC__Service_Order__c+Where+SVMXC__Component__c+=\'"+sObjectIBID+"\'";
