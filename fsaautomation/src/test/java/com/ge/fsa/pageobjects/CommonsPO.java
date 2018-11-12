7ae91a56c328e08a9f8d7e0e9a6827b9e939c87e
vinaya-svmx <vinaya.v@servicemax.com>
2018-11-12 14:46:33 +0530

    new

--- fsaautomation/src/test/java/com/ge/fsa/pageobjects/CommonsPO.java
+++ fsaautomation/src/test/java/com/ge/fsa/pageobjects/CommonsPO.java
@@ -252,7 +252,7 @@ public class CommonsPO
 			int xOff = x+100;
 			//int yOff = y-100;
 			touchAction = new TouchAction(driver);
-			touchAction.press(new PointOption().withCoordinates(x, y)).moveTo(new PointOption().withCoordinates((20), 0)).release().perform();
+			touchAction.press(new PointOption().withCoordinates(x, y)).moveTo(new PointOption().withCoordinates((20), 10)).release().perform();
 		}
 		
 		
