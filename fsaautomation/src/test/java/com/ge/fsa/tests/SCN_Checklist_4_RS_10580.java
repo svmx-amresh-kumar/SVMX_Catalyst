6e0daaecc31b32f2f9f9586699a3933ed977eb9d
vinod <vinod.tharavath@ge.com>
2019-01-08 20:03:17 +0530

    updating changes to checklist
    This reverts commit a3ba0eac41693b66709ba8e72b11ae00816cfaa4.

--- fsaautomation/src/test/java/com/ge/fsa/tests/SCN_Checklist_4_RS_10580.java
+++ fsaautomation/src/test/java/com/ge/fsa/tests/SCN_Checklist_4_RS_10580.java
@@ -26,6 +26,7 @@ public class SCN_Checklist_4_RS_10580 extends BaseLib {
 	String sWOName = null;
 	String sWOName1 = null;
 	String sWORecordID = null;
+	String sWOName3 = null;
 	
 	String sExploreChildSearchTxt = null;
 	String sOrderStatusVal = null;
@@ -105,13 +106,20 @@ public class SCN_Checklist_4_RS_10580 extends BaseLib {
 				.restGetSoqlValue("SELECT+name+from+SVMXC__Service_Order__c+Where+id+=\'" + sWORecordID + "\'", "Name");
 		System.out.println("WO no =" + sWOName1);	
 		
+		
+		// Work Order for checklist entry Criteria check.
+				 sWORecordID = restServices.restCreate("SVMXC__Service_Order__c?","{\"SVMXC__Order_Status__c\":\"Open\",\"SVMXC__Zip__c\":\"110003\",\"SVMXC__Country__c\":\"India\",\"SVMXC__State__c\":\"Haryana\",\"SVMXC__Scheduled_Date__c\":\"2018-08-28\",\"SVMXC__Scheduled_Date_Time__c\":\"2018-08-28T09:42:00.000+0000\",\"SVMXC__Idle_Time__c\":\"30\",\"SVMXC__Priority__c\":\"Low\"}");
+				System.out.println(sWORecordID);
+				sWOName3 = restServices.restGetSoqlValue("SELECT+name+from+SVMXC__Service_Order__c+Where+id+=\'" + sWORecordID + "\'", "Name");
+				System.out.println("WO no =" + sWOName3);
+		
 		bProcessCheckResult =commonsPo.ProcessCheck(restServices, genericLib, sChecklistName, sScriptName, sTestCaseID);		
 
 				
 		//sWOName1 = "WO-00001615";
 		
 	}
-	
+	//@Test()
 	@Test(retryAnalyzer=Retry.class)
 	public void RS_10580() throws Exception {
 		
@@ -152,35 +160,45 @@ public class SCN_Checklist_4_RS_10580 extends BaseLib {
 		Assert.assertTrue(checklistPo.geteleChecklistSectionNametab(sSection3Name).isDisplayed(), "Exit Criteria in Checklist Failed");	 	
 		ExtentManager.logger.log(Status.PASS,"Exit Criteria for section passed");
 		try {
-			//checklistPo.geteleChecklistSectionNametab(sSection2Name).click();
-			commonsPo.tap(checklistPo.geteleChecklistSectionNametab(sSection2Name));
+			
+			checklistPo.geteleChecklistSectionNametab(sSection2Name).click();
+			//commonsPo.tap(checklistPo.geteleChecklistSectionNametab(sSection2Name));
+			//commonsPo.doubleTap(checklistPo.geteleChecklistSectionNametab(sSection2Name));
 		} catch (Exception e) {
 			// TODO: handle exception
 			ExtentManager.logger.log(Status.PASS,"Section two is not clickable as section was jumped from 1 to 3");
 		}
 		
-		//Newly ADDED VT
 		commonsPo.tap(checklistPo.geteleChecklistSectionNametab(sSection1Name));
+		System.out.println("tapped on section one");
+		checklistPo.geteleChecklistAnsNumber(sNumberq).clear();
 		checklistPo.geteleChecklistAnsNumber(sNumberq).sendKeys(sSectionOneJump2);
 		commonsPo.tap(checklistPo.geteleSectionNextBtn(1));	
 		Assert.assertTrue(checklistPo.geteleChecklistSectionNametab(sSection3Name).isDisplayed(), "Exit Criteria in Checklist Failed");	 	
 		ExtentManager.logger.log(Status.PASS," Multiple Q same section jump --Exit Criteria for section passed");
 		try {
-			//checklistPo.geteleChecklistSectionNametab(sSection2Name).click();
-			commonsPo.tap(checklistPo.geteleChecklistSectionNametab(sSection2Name));
+			System.out.println("Entered Try");
+			checklistPo.geteleChecklistSectionNametab(sSection2Name).click();
+
+		//	commonsPo.tap(checklistPo.geteleChecklistSectionNametab(sSection2Name));
+		//	commonsPo.isDisplayedCust(checklistPo.geteleChecklistSectionNametab(sSection2Name));
 		} catch (Exception e) {
 			// TODO: handle exception
-			ExtentManager.logger.log(Status.PASS,"Multiple Q same section jump-Section two is not clickable as section was jumped from 1 to 4");
+			ExtentManager.logger.log(Status.PASS,"Multiple Q same section jump-Section two is not clickable as section was jumped");
 		}
 		
 		
 		commonsPo.tap(checklistPo.geteleChecklistSectionNametab(sSection1Name));
+		checklistPo.geteleChecklistAnsNumber(sNumberq).clear();
 		checklistPo.geteleChecklistAnsNumber(sNumberq).sendKeys("55");
 		checklistPo.geteleChecklistAnswerTextArea(sSectionOneQ1).sendKeys(sSectionOneQAns);
 		commonsPo.tap(checklistPo.geteleSectionNextBtn(1));	
+		commonsPo.tap(checklistPo.geteleChecklistOK());
+
 		try {
-			//checklistPo.geteleChecklistSectionNametab(sSection2Name).click();
-			commonsPo.tap(checklistPo.geteleChecklistSectionNametab(sSection3Name));
+			checklistPo.geteleChecklistSectionNametab(sSection3Name).click();
+			//commonsPo.isDisplayedCust(checklistPo.geteleChecklistSectionNametab(sSection3Name));
+			//commonsPo.tap(checklistPo.geteleChecklistSectionNametab(sSection3Name));
 		} catch (Exception e) {
 			// TODO: handle exception
 			ExtentManager.logger.log(Status.PASS,"Multiple Q same section jump-Section Three is not clickable as section was jumped from 1 to 4");
@@ -192,8 +210,7 @@ public class SCN_Checklist_4_RS_10580 extends BaseLib {
 		commonsPo.tap(checklistPo.geteleChecklistSectionNametab(sSection2Name));
 		commonsPo.tap(checklistPo.geteleSectionNextBtn(2));	
 
-	
-		//END of newly ADD VT
+		//commonsPo.tap(checklistPo.geteleChecklistOK());
 		commonsPo.tap(checklistPo.geteleChecklistSectionNametab(sSection3Name));
 		//checklistPo.geteleChecklistSectionNametab(sSection3Name).click();		
 		checklistPo.geteleChecklistAnsNumber(sSectionThreeErrorQ).sendKeys(sNumberDVRAns);
@@ -262,6 +279,32 @@ public class SCN_Checklist_4_RS_10580 extends BaseLib {
 			String ChecklistAnsjson = restServices.restGetSoqlValue(ChecklistQuery, "SVMXC__ChecklistJSON__c");
 			Assert.assertTrue(ChecklistAnsjson.contains(sSectionFourQ1Ans), "checklist section fourQ1  question answer is not synced to server");
 			ExtentManager.logger.log(Status.PASS,"checklist section 4 question answer text question answer is  synced to server");
+			
+			//Validating Work not satisfying checklist entry criteria		
+			System.out.println("Validating workorder not satisfying checklsit entry criteria");
+		
+			commonsPo.tap(calendarPO.getEleCalendarClick());
+			Thread.sleep(GenericLib.iLowSleep);
+			commonsPo.tap(exploreSearchPo.getEleExploreIcn());
+			workOrderPo.navigatetoWO(commonsPo, exploreSearchPo, sExploreSearch, sExploreChildSearchTxt, sWOName3);
+			
+			// Navigate to Field Service process
+			workOrderPo.selectAction(commonsPo, sFieldServiceName);
+			Thread.sleep(GenericLib.iLowSleep);
+			// Navigating to the checklist
+			commonsPo.longPress(checklistPo.geteleChecklistName(sChecklistName));
+			Thread.sleep(GenericLib.iLowSleep);
+			
+							
+			try {
+				//added to try catch because .isdisplayed does not return false when element not found.
+				Assert.assertFalse(checklistPo.geteleChecklistSectionNametab(sSection1Name).isDisplayed());
+				ExtentManager.logger.log(Status.FAIL,"Entry Criteria for checklist is failure");
+
+			} catch (Exception e) {
+				System.out.println("Entered catch block to validate entryt criteria");
+				ExtentManager.logger.log(Status.PASS,"Entry Criteria for checklist is validated sucessfully");
+			}				
 			
 	
 }
