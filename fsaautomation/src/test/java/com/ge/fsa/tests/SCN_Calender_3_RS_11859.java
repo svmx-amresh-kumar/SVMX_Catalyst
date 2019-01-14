4b0ac755aa98608016865e5adadbe538c0e42712
vinaya-svmx <vinaya.v@servicemax.com>
2019-01-14 11:22:25 +0530

    Update SCN_Mapping_RS_10556.java

--- fsaautomation/src/test/java/com/ge/fsa/tests/SCN_Calender_3_RS_11859.java
+++ fsaautomation/src/test/java/com/ge/fsa/tests/SCN_Calender_3_RS_11859.java
@@ -63,17 +63,17 @@ public class SCN_Calender_3_RS_11859 extends BaseLib {
 		
 		String sTestCaseID="RS_11859_Calender_3";
 		//sahi
-		/*
-		 * genericLib.executeSahiScript("appium/SCN_Calender_3_RS-11859.sah",
-		 * "sTestCaseID"); if(commonsPo.verifySahiExecution()) {
-		 * 
-		 * System.out.println("PASSED"); } else { System.out.println("FAILED");
-		 * 
-		 * 
-		 * ExtentManager.logger.log(Status.FAIL,"Testcase " + sTestCaseID +
-		 * "Sahi verification failure"); assertEquals(0, 1); } lauchNewApp("true");
-		 * System.out.println("RS-11859");
-		 */
+		
+		  genericLib.executeSahiScript("appium/SCN_Calender_3_RS-11859.sah",
+		  "sTestCaseID"); if(commonsPo.verifySahiExecution()) {
+		  
+		  System.out.println("PASSED"); } else { System.out.println("FAILED");
+		  
+		  
+		  ExtentManager.logger.log(Status.FAIL,"Testcase " + sTestCaseID +
+		  "Sahi verification failure"); assertEquals(0, 1); } lauchNewApp("true");
+		  System.out.println("RS-11859");
+		 
 	
 	//read from file
 		sExploreSearch = GenericLib.getExcelData(sTestCaseID,sSheetName, "ExploreSearch");
@@ -84,105 +84,104 @@ public class SCN_Calender_3_RS_11859 extends BaseLib {
 		String sWO_SFDC_2 = GenericLib.getExcelData(sTestCaseID,sSheetName, "WO_SFDC_2");
 		
 		
-		/*
-		 * //Pre Login to app loginHomePo.login(commonsPo, exploreSearchPo);
-		 * 
-		 * //config sync toolsPo.configSync(commonsPo);
-		 * Thread.sleep(GenericLib.iMedSleep);
-		 * 
-		 * 
-		 * //Data Sync for WO's created toolsPo.syncData(commonsPo);
-		 * Thread.sleep(GenericLib.iMedSleep);
-		 * 
-		 * /////////////////////////////////////////////////////////////////////////////
-		 * //////////////////////////////////////////////// //verify WO event is present
-		 * or not commonsPo.tap(calendarPO.getEleCalendarClick()); Thread.sleep(3000);
-		 * commonsPo.tap(calendarPO.getEleCalendarClick()); Thread.sleep(3000);
-		 * calendarPO.VerifyWOInCalender(commonsPo,sWO_SFDC_1);
-		 * calendarPO.VerifyWOInCalender(commonsPo,sWO_SFDC_2);
-		 * 
-		 * ExtentManager.logger.log(Status.
-		 * PASS,"Two SFDC events are displayed in calendar");
-		 * 
-		 * System.out.println(
-		 * "//////////////////////////////////////////////////////////////////////////////////////////////"
-		 * );
-		 * /////////////////////////////////////////////////////////////////////////////
-		 * /////////////////////////////// //Create SFDC event from Create New Option
-		 * commonsPo.tap(calendarPO.getEleCalendarClick()); Thread.sleep(3000);
-		 * commonsPo.tap(calendarPO.geteleNewClick());
-		 * 
-		 * commonsPo.tap(calendarPO.getelesubjectSFDCtap(),20,20);
-		 * 
-		 * calendarPO.getelesubjectcal().sendKeys("Create SFDC event from new button");
-		 * //commonsPo.tap(calendarPO.geteleclickupdate());
-		 * commonsPo.setDateTime24hrs(calendarPO.geteleStartDateTimecal(), 0,"10",
-		 * "00"); //set start time to Today
-		 * commonsPo.setDateTime24hrs(calendarPO.geteleEndDateTimecal(), 0,"15","00");
-		 * 
-		 * commonsPo.tap(workOrderPo.getEleClickSave());
-		 * 
-		 * toolsPo.syncData(commonsPo);
-		 * 
-		 * sObjectApi = "Event"; restServices.getAccessToken(); sSqlEventQuery
-		 * ="SELECT+Id+from+Event+Where+Subject+=\'Create SFDC event from new button\'";
-		 * sEventIdSFDC =restServices.restGetSoqlValue(sSqlEventQuery,"Id");
-		 * System.out.println("created event id from server:"+sEventIdSFDC);
-		 * Assert.assertNotNull(sEventIdSFDC, "Record not found");;
-		 * ExtentManager.logger.log(Status.
-		 * PASS,"Create SVMX event from Create New Option is Successful");
-		 * System.out.println(
-		 * "//////////////////////////////////////////////////////////////////////////////////////////////"
-		 * );
-		 * 
-		 * /////////////////////////////////////////////////////////////////////////////
-		 * /////////////////////////////////////////// // On server/DC, edit one of the
-		 * events created
-		 * 
-		 * sObjectApi = "Event"; sSqlEventQuery
-		 * ="SELECT+id+from+Event+Where+Subject+=\'A11859_SFDC_Event1\'"; sEventIdSVMX_1
-		 * =restServices.restGetSoqlValue(sSqlEventQuery,"Id");
-		 * System.out.println(sEventIdSVMX_1);
-		 * 
-		 * SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-		 * Calendar now1 = Calendar.getInstance(); now1.set(Calendar.HOUR, 12);
-		 * now1.set(Calendar.MINUTE, 0); now1.set(Calendar.SECOND, 0); String
-		 * endtimezero=sdf.format(now1.getTime()); // now1.set(Calendar.HOUR_OF_DAY,
-		 * 14); System.out.println(endtimezero);
-		 * 
-		 * String sWOJson="{\"EndDateTime\":\""+endtimezero+"\"}";
-		 * restServices.restUpdaterecord(sObjectApi,sWOJson,sEventIdSVMX_1);
-		 * 
-		 * toolsPo.syncData(commonsPo);
-		 * 
-		 * String stempDate=calendarPO.convertedformate(endtimezero);
-		 * 
-		 * commonsPo.tap(calendarPO.getEleCalendarClick()); Thread.sleep(3000);
-		 * 
-		 * calendarPO.geteleWOendpoint("09:00").getLocation(); Thread.sleep(3000);
-		 * System.out.println("Before Pencil icon enable"); try {
-		 * commonsPo.Enablepencilicon(calendarPO.getelegetsubject(sWO_SFDC_1)); //tap on
-		 * pencil icon System.out.println("tap on pencil icon");
-		 * commonsPo.tap(calendarPO.getelepenciliconcal(sWO_SFDC_1),20,20); String
-		 * EndDateTimecal=calendarPO.geteleEndDateTime().getAttribute("value");//dummy }
-		 * catch (Exception e) {
-		 * commonsPo.Enablepencilicon(calendarPO.getsubjectformultiday(sWO_SFDC_1));
-		 * commonsPo.tap(calendarPO.getelepenciliconcalmultiday(sWO_SFDC_1),20,20);
-		 * Thread.sleep(3000); }
-		 * 
-		 * 
-		 * String EndDateTimecal=calendarPO.geteleEndDateTime().getAttribute("value");
-		 * System.out.println(EndDateTimecal);
-		 * 
-		 * Assert.assertEquals(stempDate,EndDateTimecal,
-		 * "End Date time value mapped is not displayed");
-		 * commonsPo.tap(workOrderPo.getEleCancelLnk());
-		 * ExtentManager.logger.log(Status.
-		 * PASS,"On server/DC, edit one of the events and validated in client is successful"
-		 * ); System.out.println(
-		 * "//////////////////////////////////////////////////////////////////////////////////////////////"
-		 * );
-		 */
+		
+		  //Pre Login to app 
+		loginHomePo.login(commonsPo, exploreSearchPo);
+		  
+		  //config sync 
+		toolsPo.configSync(commonsPo);
+		  Thread.sleep(GenericLib.iMedSleep);
+		  
+		  
+		  //Data Sync for WO's created 
+		  toolsPo.syncData(commonsPo);
+		  Thread.sleep(GenericLib.iMedSleep);
+		  
+		  /////////////////////////////////////////////////////////////////////////////
+		  //////////////////////////////////////////////// //verify WO event is presentor not 
+		  commonsPo.tap(calendarPO.getEleCalendarClick()); Thread.sleep(3000);
+		  commonsPo.tap(calendarPO.getEleCalendarClick()); Thread.sleep(3000);
+		  calendarPO.VerifyWOInCalender(commonsPo,sWO_SFDC_1);
+		  calendarPO.VerifyWOInCalender(commonsPo,sWO_SFDC_2);
+		  
+		  ExtentManager.logger.log(Status.
+		  PASS,"Two SFDC events are displayed in calendar");
+		  
+		  System.out.println(
+		  "//////////////////////////////////////////////////////////////////////////////////////////////"
+		  );
+		  /////////////////////////////////////////////////////////////////////////////
+		  /////////////////////////////// //Create SFDC event from Create New Option
+		  commonsPo.tap(calendarPO.getEleCalendarClick()); Thread.sleep(3000);
+		  commonsPo.tap(calendarPO.geteleNewClick());
+		  
+		  commonsPo.tap(calendarPO.getelesubjectSFDCtap(),20,20);
+		  
+		  calendarPO.getelesubjectcal().sendKeys("Create SFDC event from new button");
+		  //commonsPo.tap(calendarPO.geteleclickupdate());
+		  commonsPo.setDateTime24hrs(calendarPO.geteleStartDateTimecal(), 0,"10",
+		  "00"); //set start time to Today
+		  commonsPo.setDateTime24hrs(calendarPO.geteleEndDateTimecal(), 0,"15","00");
+		  
+		  commonsPo.tap(workOrderPo.getEleClickSave());
+		  
+		  toolsPo.syncData(commonsPo);
+		  
+		  sObjectApi = "Event"; restServices.getAccessToken(); sSqlEventQuery
+		  ="SELECT+Id+from+Event+Where+Subject+=\'Create SFDC event from new button\'";
+		  sEventIdSFDC =restServices.restGetSoqlValue(sSqlEventQuery,"Id");
+		  System.out.println("created event id from server:"+sEventIdSFDC);
+		  Assert.assertNotNull(sEventIdSFDC, "Record not found");;
+		  ExtentManager.logger.log(Status.
+		  PASS,"Create SVMX event from Create New Option is Successful");
+		  System.out.println(
+		  "//////////////////////////////////////////////////////////////////////////////////////////////"
+		  );
+		  
+		  /////////////////////////////////////////////////////////////////////////////
+		  /////////////////////////////////////////// // On server/DC, edit one of the events created
+		  
+		  sObjectApi = "Event"; sSqlEventQuery
+		  ="SELECT+id+from+Event+Where+Subject+=\'A11859_SFDC_Event1\'"; sEventIdSVMX_1
+		  =restServices.restGetSoqlValue(sSqlEventQuery,"Id");
+		  System.out.println(sEventIdSVMX_1);
+		  
+		  SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+		  Calendar now1 = Calendar.getInstance(); now1.set(Calendar.HOUR, 12);
+		  now1.set(Calendar.MINUTE, 0); now1.set(Calendar.SECOND, 0); 
+		  String endtimezero=sdf.format(now1.getTime()); // now1.set(Calendar.HOUR_OF_DAY,14); 
+		  System.out.println(endtimezero);
+		  
+		  String sWOJson="{\"EndDateTime\":\""+endtimezero+"\"}";
+		  restServices.restUpdaterecord(sObjectApi,sWOJson,sEventIdSVMX_1);
+		  
+		  toolsPo.syncData(commonsPo);
+		  
+		  String stempDate=calendarPO.convertedformate(endtimezero);
+		  
+		  commonsPo.tap(calendarPO.getEleCalendarClick()); Thread.sleep(3000);
+		  
+		  calendarPO.geteleWOendpoint("09:00").getLocation(); Thread.sleep(3000);
+		  System.out.println("Before Pencil icon enable"); try {
+		  commonsPo.Enablepencilicon(calendarPO.getelegetsubject(sWO_SFDC_1)); //tap onpencil icon 
+		  System.out.println("tap on pencil icon");
+		  commonsPo.tap(calendarPO.getelepenciliconcal(sWO_SFDC_1),20,20); String
+		  EndDateTimecal=calendarPO.geteleEndDateTime().getAttribute("value");//dummy 
+		  }
+		  catch (Exception e) {
+		  commonsPo.Enablepencilicon(calendarPO.getsubjectformultiday(sWO_SFDC_1));
+		  commonsPo.tap(calendarPO.getelepenciliconcalmultiday(sWO_SFDC_1),20,20);
+		  Thread.sleep(3000); }
+		  
+		  
+		  String EndDateTimecal=calendarPO.geteleEndDateTime().getAttribute("value");
+		  System.out.println(EndDateTimecal);
+		  
+		  Assert.assertEquals(stempDate,EndDateTimecal,"End Date time value mapped is not displayed");
+		  commonsPo.tap(workOrderPo.getEleCancelLink());
+		  ExtentManager.logger.log(Status.PASS,"On server/DC, edit one of the events and validated in client is successful"); 
+		  System.out.println("//////////////////////////////////////////////////////////////////////////////////////////////" );
+		 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	//	On client, edit one of the events created 
@@ -212,14 +211,14 @@ public class SCN_Calender_3_RS_11859 extends BaseLib {
 		
 		
 		
-		/*
-		 * System.out.println("tap on pencil icon");
-		 * commonsPo.tap(calendarPO.getelepenciliconcal(sWO_SFDC_1),20,20);
-		 */
+		
+		 // System.out.println("tap on pencil icon");
+		//  commonsPo.tap(calendarPO.getelepenciliconcal(sWO_SFDC_1),20,20);
+		 
 
 			Thread.sleep(10000);
-		//commonsPo.tap(calendarPO.geteleEndDateTime());
-		//commonsPo.setDateTime24hrs(calendarPO.geteleEndDateTime(), 0,"15","00");
+		commonsPo.tap(calendarPO.geteleEndDateTime());
+		commonsPo.setDateTime24hrs(calendarPO.geteleEndDateTime(), 0,"15","00");
 	
 		commonsPo.switchContext("Native");
 		commonsPo.getEleDonePickerWheelBtn().click();
@@ -248,49 +247,49 @@ public class SCN_Calender_3_RS_11859 extends BaseLib {
 		String sEventenddatetimeserver =restServices.restGetSoqlValue(sSqlEventQuery,"EndDateTime"); 
 		System.out.println(sEventenddatetimeserver);
 		
-		/*
-		 * stempDate = calendarPO.convertedformate(sEventenddatetimeserver);
-		 * 
-		 * Assert.assertEquals(stempDate,EndDateTimevalidate,
-		 * "End Date time  mismatch"); ExtentManager.logger.log(Status.
-		 * PASS," On client, edit one of the event is successful"); System.out.println(
-		 * "//////////////////////////////////////////////////////////////////////////////////////////////"
-		 * );
-		 * /////////////////////////////////////////////////////////////////////////////
-		 * ////////////////////// //On client, create an SFDC event longer than 14 days.
-		 * 
-		 * 
-		 * commonsPo.tap(calendarPO.getEleCalendarClick()); Thread.sleep(3000);
-		 * commonsPo.tap(calendarPO.geteleNewClick());
-		 * //commonsPo.tap(calendarPO.getelesubjectSFDCtap(),20,20);
-		 * 
-		 * calendarPO.getelesubjectSFDCtap().sendKeys("Event for more than 14 days");
-		 * //commonsPo.tap(calendarPO.geteleclickupdate());
-		 * commonsPo.setDateTime24hrs(calendarPO.geteleStartDateTimecal(), 0,"10",
-		 * "00"); //set start time to Today
-		 * commonsPo.setDateTime24hrs(calendarPO.geteleEndDateTimecal(), 18,"10","00");
-		 * 
-		 * commonsPo.tap(workOrderPo.getEleClickSave());
-		 * 
-		 * commonsPo.tap(toolsPo.getEleToolsIcn());
-		 * toolsPo.getEleSyncDataNowLnk().click();
-		 * commonsPo.tap(toolsPo.getEleSyncDataNowLnk());
-		 * toolsPo.getEleStartSyncBtn().click();
-		 * commonsPo.longPress(toolsPo.getEleStartSyncBtn());
-		 * commonsPo.waitforElement(toolsPo.getEleRefreshingViewTxt(), 120000);
-		 * 
-		 * commonsPo.tap(toolsPo.geteleResolve()); String
-		 * errormsg=toolsPo.getelesyncconflicterror().getText();
-		 * System.out.println(errormsg); //Assert.assertEquals(
-		 * errormsg,"An event can't last longer than 14 days.: Duration");
-		 * Assert.assertEquals(errormsg,"An event can't last longer than 14 days.");
-		 * ExtentManager.logger.log(Status.
-		 * PASS," On client, create an SVMX event longer than 14 days is successful");
-		 * 
-		 * commonsPo.tap(toolsPo.geteleResolveissue());
-		 * commonsPo.tap(toolsPo.geteleApply()); commonsPo.tap(toolsPo.getEleOkBtn());
-		 * toolsPo.syncData(commonsPo);
-		 */
+		
+		  stempDate = calendarPO.convertedformate(sEventenddatetimeserver);
+		  
+		  Assert.assertEquals(stempDate,EndDateTimevalidate,
+		  "End Date time  mismatch"); ExtentManager.logger.log(Status.
+		  PASS," On client, edit one of the event is successful"); System.out.println(
+		  "//////////////////////////////////////////////////////////////////////////////////////////////"
+		  );
+		  /////////////////////////////////////////////////////////////////////////////
+		  ////////////////////// //On client, create an SFDC event longer than 14 days.
+		  
+		  
+		  commonsPo.tap(calendarPO.getEleCalendarClick()); Thread.sleep(3000);
+		  commonsPo.tap(calendarPO.geteleNewClick());
+		  //commonsPo.tap(calendarPO.getelesubjectSFDCtap(),20,20);
+		  
+		  calendarPO.getelesubjectSFDCtap().sendKeys("Event for more than 14 days");
+		  //commonsPo.tap(calendarPO.geteleclickupdate());
+		  commonsPo.setDateTime24hrs(calendarPO.geteleStartDateTimecal(), 0,"10",
+		  "00"); //set start time to Today
+		  commonsPo.setDateTime24hrs(calendarPO.geteleEndDateTimecal(), 18,"10","00");
+		  
+		  commonsPo.tap(workOrderPo.getEleClickSave());
+		  
+		  commonsPo.tap(toolsPo.getEleToolsIcn());
+		  toolsPo.getEleSyncDataNowLnk().click();
+		  commonsPo.tap(toolsPo.getEleSyncDataNowLnk());
+		  toolsPo.getEleStartSyncBtn().click();
+		  commonsPo.longPress(toolsPo.getEleStartSyncBtn());
+		  commonsPo.waitforElement(toolsPo.getEleRefreshingViewTxt(), 120000);
+		  
+		  commonsPo.tap(toolsPo.geteleResolve()); String
+		  errormsg=toolsPo.getelesyncconflicterror().getText();
+		  System.out.println(errormsg); 
+		  //Assert.assertEquals(errormsg,"An event can't last longer than 14 days.: Duration");
+		  Assert.assertEquals(errormsg,"An event can't last longer than 14 days.");
+		  ExtentManager.logger.log(Status.
+		  PASS," On client, create an SVMX event longer than 14 days is successful");
+		  
+		  commonsPo.tap(toolsPo.geteleResolveissue());
+		  commonsPo.tap(toolsPo.geteleApply()); commonsPo.tap(toolsPo.getEleOkBtn());
+		  toolsPo.syncData(commonsPo);
+		 
         System.out.println("//////////////////////////////////////////////////////////////////////////////////////////////");
 	}
 	
