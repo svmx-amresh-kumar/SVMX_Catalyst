1b7f09bfda496f421c50401fb4cb932746bf7b73
harishsvmx <harish.cs@ge.com>
2019-01-24 11:19:52 +0530

    Uncommented retry analyser and execute sahi

--- fsaautomation/src/test/java/com/ge/fsa/tests/SCN_CustomAction_2_RS_10558.java
+++ fsaautomation/src/test/java/com/ge/fsa/tests/SCN_CustomAction_2_RS_10558.java
@@ -17,10 +17,10 @@ public class SCN_CustomAction_2_RS_10558 extends BaseLib {
 	String sProcessName = "10558_Action";
 	String sScriptName = "SCN_CustomAction_RS_10558";
 	String sTestCaseID = "RS_10558";
-	@Test//(retryAnalyzer=Retry.class)
+	@Test(retryAnalyzer=Retry.class)
 	public void RS_10558() throws Exception {
 		
-//		commonsPo.execSahi(genericLib, sScriptName, sTestCaseID);
+		commonsPo.execSahi(genericLib, sScriptName, sTestCaseID);
 		
 		String sWORecordID = restServices.restCreate("SVMXC__Service_Order__c?","{}");
 //		System.out.println(sWORecordID);
