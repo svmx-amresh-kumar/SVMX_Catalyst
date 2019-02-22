24911c8213c91fdbe47195110b3d7dbf1fffd13b
Yadav Thyagaraj <yadav.thyagaraj@servicemax.com>
2019-02-22 13:07:49 +0530

    re

--- Executable/sahiExecutable.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash 
-cd /auto/sahi_pro/userdata/bin 
-./testrunner.sh /auto/sahi_pro/userdata/scripts/Sahi_Project_Lightning/svmx/test_lab/test_cases/appium/Scenario_10570.sah https://test.salesforce.com chrome
