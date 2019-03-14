5bcb798009478cbf22c18bf5965e7cf570f320fb
vinod <vinod.tharavath@ge.com>
2019-03-14 14:53:34 +0530

    Update CommonsPO.java

--- fsaautomation/src/test/java/com/ge/fsa/pageobjects/CommonsPO.java
+++ fsaautomation/src/test/java/com/ge/fsa/pageobjects/CommonsPO.java
@@ -49,6 +49,7 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.android.AndroidTouchAction;
 import io.appium.java_client.ios.IOSElement;
@@ -1700,7 +1701,9 @@ public class CommonsPO {
 			System.out.println("Scrolling to : "+sValue);
 			switch(BaseLib.sOSName.toLowerCase()) {
 			case "android":
-				driver.findElement(MobileBy.AndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().text(\""+sValue+"\"))"));
+				try {
+				driver.findElement(MobileBy.AndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true).scrollable(true)).scrollIntoView(new UiSelector().className(\"android.widget.ViewGroup\").enabled(true).instance(10))"));//"new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().text(\""+sValue+"\"))"));
+				}catch(Exception e) {}
 				break;
 			case "ios":
 				JavascriptExecutor js = (JavascriptExecutor) driver;
@@ -1711,7 +1714,7 @@ public class CommonsPO {
 					params.put("direction", "down");
 
 				}
-				params.put("element", ((RemoteWebElement) webElement).getId());
+				//params.put("element", ((RemoteWebElement) webElement).getId());
 				js.executeScript("mobile: swipe", params);
 		
 			//FYI No need to pass element, the app window will be chosen
@@ -1723,10 +1726,14 @@ public class CommonsPO {
 		}
 		
 		
-		public void custScrollToElement(WebElement el) {
-			
-			//for(;;)
-			//swipeGeneric("up");
+		public void custScrollToElementAndClick(WebElement webElement) {
+			int i;
+			for(i=0;i<10;i++)
+			{
+				swipeGeneric(webElement,"up");
+				webElement.click();
+			}
+			//
 			
 		}
 }
