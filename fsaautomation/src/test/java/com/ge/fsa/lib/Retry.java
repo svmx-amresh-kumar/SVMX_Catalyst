e8670a3428f3009a606b472810559defe0f202a9
Yadav Thyagaraj <yadav.thyagaraj@servicemax.com>
2019-04-19 21:52:19 -0700

    Update Retry.java

--- fsaautomation/src/test/java/com/ge/fsa/lib/Retry.java
+++ fsaautomation/src/test/java/com/ge/fsa/lib/Retry.java
@@ -20,7 +20,7 @@ public class Retry implements IRetryAnalyzer {
 	// override retry Method
 
 	public boolean retry(ITestResult result) {
-
+		isRetryRun = false;
 		// this will run until max count completes if test pass within this frame it
 		// will come out of for loop
 
