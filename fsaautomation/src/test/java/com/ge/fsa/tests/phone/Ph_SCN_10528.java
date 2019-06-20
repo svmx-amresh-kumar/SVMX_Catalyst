46c6d2731b1abb75ad497584c75283f5f03dfd4e
harishsvmx <harish.cs@ge.com>
2019-06-20 12:42:26 +0530

    Fixed Review Comments for 10528

--- fsaautomation/src/test/java/com/ge/fsa/tests/phone/Ph_SCN_10528.java
+++ fsaautomation/src/test/java/com/ge/fsa/tests/phone/Ph_SCN_10528.java
@@ -10,7 +10,9 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.Status;
 import com.ge.fsa.lib.BaseLib;
+import com.ge.fsa.lib.ExtentManager;
 import com.ge.fsa.lib.GenericLib;
 import com.ge.fsa.lib.Retry;
 
@@ -25,11 +27,13 @@ public class Ph_SCN_10528 extends BaseLib {
 	String sZipCode = "51203";
 	String sCountry = "India";
 	String sCity = "Bangalore";
+	Boolean bProcessCheckResult;
+	String sProcessName = "AutoReg10528";
 	
-	@Test(retryAnalyzer=Retry.class)
+	@Test//(retryAnalyzer=Retry.class)
 	public void RS_10528() throws Exception {
 	
-	commonUtility.execSahi(genericLib, sScriptName, sTestCaseID);
+	bProcessCheckResult = commonUtility.ProcessCheck(restServices, genericLib, sProcessName, sScriptName,sTestCaseID);
 	
 // **********Create Account**********
     String sAccName = commonUtility.generateRandomNumber("Acc");
@@ -77,16 +81,20 @@ public class Ph_SCN_10528 extends BaseLib {
 	ph_LoginHomePo.login(commonUtility, ph_MorePo);
 	
 	ph_MorePo.syncData(commonUtility);
-	ph_MorePo.configSync(commonUtility, ph_CalendarPo);
 	
-	ph_ExploreSearchPo.navigateToSFM(commonUtility, ph_WorkOrderPo,  "AUTOMATION SEARCH", "Work Orders",sWOName,"AutoReg10528");
+	
+	ph_ExploreSearchPo.navigateToSFM(commonUtility, ph_WorkOrderPo,  "AUTOMATION SEARCH", "Work Orders",sWOName,sProcessName);
 	ph_CreateNewPo.selectFromlookupSearchList(commonUtility, ph_WorkOrderPo.getLblComponent(), sIbName1);
 	
 	// **********Assert if Form Fill is happening correctly*****************
 		Assert.assertEquals(ph_WorkOrderPo.getTxtProduct().getText(), sProdName);
+		ExtentManager.logger.log(Status.PASS, "Actual Product: "+ph_WorkOrderPo.getTxtProduct().getText()+" Expected Product: "+sProdName);
 		Assert.assertEquals(ph_WorkOrderPo.getTxtZip().getText(), sZipCode);
+		ExtentManager.logger.log(Status.PASS, "Actual ZipCode: "+ph_WorkOrderPo.getTxtZip().getText()+" Expected ZipCode: "+sZipCode);
 		Assert.assertEquals(ph_WorkOrderPo.getTxtTopLevel().getText(), sIbName);
+		ExtentManager.logger.log(Status.PASS, "Actual Top Level: "+ph_WorkOrderPo.getTxtTopLevel().getText()+" Expected Top Level: "+sIbName);
 		Assert.assertEquals(ph_WorkOrderPo.getTxtSite().getText(), sLocName);
+		ExtentManager.logger.log(Status.PASS, "Actual Location: "+ph_WorkOrderPo.getTxtSite().getText()+" Expected Location: "+sLocName);
 	Dimension dim = driver.manage().window().getSize();
 	int height = dim.getHeight();
 	int width = dim.getWidth();
@@ -96,32 +104,34 @@ public class Ph_SCN_10528 extends BaseLib {
 	ts.press(new PointOption().withCoordinates(x, y)).waitAction(WaitOptions.waitOptions(Duration.ofMillis(2000))).moveTo(new PointOption().withCoordinates(x, height-10)).release().perform();
 	Thread.sleep(2000); 
 		Assert.assertEquals(ph_WorkOrderPo.getTxtContact().getText(), sContactName);
+		ExtentManager.logger.log(Status.PASS, "Actual Contact: "+ph_WorkOrderPo.getTxtContact().getText()+" Expected Contact: "+sContactName);
 		Assert.assertEquals(ph_WorkOrderPo.getTxtCountry().getText(), sCountry);
+		ExtentManager.logger.log(Status.PASS, "Actual Country: "+ph_WorkOrderPo.getTxtCountry().getText()+" Expected Country: "+sCountry);
 		Assert.assertEquals(ph_WorkOrderPo.getTxtCity().getText(), sCity);
+		ExtentManager.logger.log(Status.PASS, "Actual City: "+ph_WorkOrderPo.getTxtCity().getText()+" Expected City: "+sCity);
 	// **********************************************************************
 	Thread.sleep(10000);
 	ph_WorkOrderPo.getBtnSave().click();
 	ph_MorePo.getEleMoreBtn().click();
-//	ph_MorePo.configSync(commonUtility, ph_CalendarPo);--Remove
 	ph_MorePo.syncData(commonUtility);
 	// **********Assert if Database is updated correctly after Save**********
 	String sDBCity = restServices.restGetSoqlValue("SELECT+SVMXC__City__c+from+SVMXC__Service_Order__c+Where+name+=\'"+sWOName+"\'", "SVMXC__City__c");
-	System.out.println(sDBCity);
 	Assert.assertEquals(sDBCity, sCity);
+	ExtentManager.logger.log(Status.PASS, "Actual City from DB: "+sDBCity+" Expected City: "+sCity);
 	String sDBZip = restServices.restGetSoqlValue("SELECT+SVMXC__Zip__c+from+SVMXC__Service_Order__c+Where+name+=\'"+sWOName+"\'", "SVMXC__Zip__c");
-	System.out.println(sDBZip);
 	Assert.assertEquals(sDBZip, sZipCode);
+	ExtentManager.logger.log(Status.PASS, "Actual ZipCode from DB: "+sDBZip+" Expected ZipCode: "+sZipCode);
 	String sDBCountry = restServices.restGetSoqlValue("SELECT+SVMXC__Country__c+from+SVMXC__Service_Order__c+Where+name+=\'"+sWOName+"\'", "SVMXC__Country__c");
-	System.out.println(sDBCountry);
 	Assert.assertEquals(sDBCountry, sCountry);
+	ExtentManager.logger.log(Status.PASS, "Actual Country from DB: "+sDBCountry+" Expected Country: "+sCountry);
 	String sDBContactId = restServices.restGetSoqlValue("SELECT+SVMXC__Contact__c+from+SVMXC__Service_Order__c+Where+name+=\'"+sWOName+"\'", "SVMXC__Contact__c");
-	System.out.println(sDBContactId);
 	String sDBContact = restServices.restGetSoqlValue("SELECT+name+from+Contact+Where+id+=\'"+sDBContactId+"\'", "Name");
 	Assert.assertEquals(sDBContact, sContactName);
+	ExtentManager.logger.log(Status.PASS, "Actual Contact from DB: "+sDBContact+" Expected Contact: "+sContactName);
 	String sDBProductId = restServices.restGetSoqlValue("SELECT+SVMXC__Product__c+from+SVMXC__Service_Order__c+Where+name+=\'"+sWOName+"\'", "SVMXC__Product__c");
-	System.out.println(sDBProductId);
 	String sDBProduct = restServices.restGetSoqlValue("SELECT+name+from+Product2+Where+id+=\'"+sDBProductId+"\'", "Name");
 	Assert.assertEquals(sDBProduct, sProdName);
+	ExtentManager.logger.log(Status.PASS, "Actual Product from DB: "+sDBProduct+" Expected Product: "+sProdName);
 	// **********************************************************************
 	
 	
