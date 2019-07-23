9485668a7fe37ae9ac476a556b6309f75d136ecb
Yadav Thyagaraj <yadav.thyagaraj@servicemax.com>
2019-07-23 14:45:29 +0530

    added links

--- fsaautomation/src/test/java/com/ge/fsa/tests/tablet/SCN_ChecklistOPDOC_RS_10587.java
+++ fsaautomation/src/test/java/com/ge/fsa/tests/tablet/SCN_ChecklistOPDOC_RS_10587.java
@@ -100,6 +100,14 @@ public class SCN_ChecklistOPDOC_RS_10587 extends BaseLib {
 	@Test(retryAnalyzer=Retry.class)
 	//@Test()
 	public void RS_10587() throws Exception {
+		// JiraLink
+		if (BaseLib.sOSName.equalsIgnoreCase("ios")) {
+			commonUtility.addLinkInExtentReport("https://servicemax.atlassian.net/browse/RS-10587");
+		} else {
+			commonUtility.addLinkInExtentReport("https://servicemax.atlassian.net/browse/RS-12055");
+
+		}
+
 		prerequisites();
 		
 		// Pre Login to app
